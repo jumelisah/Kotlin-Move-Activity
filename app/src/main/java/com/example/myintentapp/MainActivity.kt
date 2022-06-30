cee2c720d678a229b55b5fc7c42681d0c8756481
juumelisa <jumelisa09@gmail.com>
2022-06-30 14:34:27 +0700

    implicit intent

--- app/src/main/java/com/example/myintentapp/MainActivity.kt
+++ app/src/main/java/com/example/myintentapp/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.content.Intent
+import android.net.Uri
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +17,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         val btnMoveActivityData: Button = findViewById(R.id.btn_move_activity_data)
         btnMoveActivityData.setOnClickListener(this)
+
+        val btnDialNumber: Button = findViewById(R.id.btn_dial_number)
+        btnDialNumber.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -29,6 +33,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "Jumelisah")
                 moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 24)
                 startActivity(moveWithDataIntent)
+            }
+            R.id.btn_dial_number -> {
+                val phoneNumber = "082177928366"
+                val dialPhoneIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber"))
+                startActivity(dialPhoneIntent)
             }
         }
     }
