ad07dcb3803691c773c7e0608d442ef4bbe04371
juumelisa <jumelisa09@gmail.com>
2022-06-30 14:24:13 +0700

    move activity

---
+++ app/src/main/java/com/example/myintentapp/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.myintentapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.content.Intent
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnMoveActivity: Button = findViewById(R.id.btn_move_activity)
+        btnMoveActivity.setOnClickListener(this)
+
+        val btnMoveActivityData: Button = findViewById(R.id.btn_move_activity_data)
+        btnMoveActivityData.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_move_activity -> {
+                val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
+                startActivity(moveIntent)
+            }
+            R.id.btn_move_activity_data -> {
+                val moveWithDataIntent = Intent(this@MainActivity, MoveWithDataActivity::class.java)
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "Jumelisah")
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 24)
+                startActivity(moveWithDataIntent)
+            }
+        }
+    }
+}
